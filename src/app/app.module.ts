1d50180cd1fe084b123a64f4ddb1304ccbaefbc4
Sebastian Brown <seb@sebbrown.net>
2020-09-23 16:25:10 +1000

    routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MazeComponent } from './maze/maze.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MazeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MazeComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/maze/maze.component.ts
+++ src/app/maze/maze.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-maze',
   templateUrl: './maze.component.html',
-  styleUrls: ['./maze.component.scss']
+  styleUrls: ['./maze.component.scss'],
 })
 export class MazeComponent implements OnInit {
-
-  constructor() { }
+  loading = false;
+  moves = 0;
+  constructor() {}
 
   ngOnInit(): void {
+    console.log('Maze Init!');
   }
-
 }
